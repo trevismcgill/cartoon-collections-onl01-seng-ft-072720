0d747f7667c2aceece1f9910e25c3ed2c087a9fe
Trevis McGill <trevismcgill25@gmail.com>
2020-07-30 00:58:38 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  adjusted_array = []
+  array.collect do |element|
+    adjusted_array.push(element.capitalize + "!")
+  end
+  adjusted_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |calls|
+    if calls.length > 4
+      true
+    else
+      false
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect do |cheese|
+  cheese_types.include?(cheese)
+  end
 end
